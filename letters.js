7905416efd364b3afaa481c83f0d5cfbb628e707
Michael Aphayvong <103137983+robbzombiee@users.noreply.github.com>
2023-09-12 14:39:10 +1200

    abcd update
    abcd update

--- letters.js
+++ letters.js
@@ -21,15 +21,66 @@ const alphabet = {
     "arcStart" : 0,
     "arcEnd" : 360,
 
-    "circX" : 50,
-    "circY" : 96,
-    "circSize" : 36,
+    "circX" : 73,
+    "circY" : 100,
+    "circSize" : 18.5,
 
-    "rectX" : 73,
-    "rectY" : 115.99,
-    "rectWidth" : 8,
-    "rectHeight" : 21,
-    "rectRotate" : 133
+    "rectX" : 81,
+    "rectY" : 106,
+    "rectWidth" : 32,
+    "rectHeight" : 8,
+    "rectRotate" : 259.2
+
+  },
+  
+  "B" : {
+
+    "arcStart" : 0,
+    "arcEnd" : 360,
+
+    "circX" : 28.04,
+    "circY" : 102,
+    "circSize" : 20,
+
+    "rectX" : 20,
+    "rectY" : 76,
+    "rectWidth" : 102,
+    "rectHeight" : 7.01,
+    "rectRotate" : 270
+
+  },
+
+  "C" : {
+
+    "arcStart" : 0,
+    "arcEnd" : 360,
+
+    "circX" : 50,
+    "circY" : 100,
+    "circSize" : 27,
+
+    "rectX" : 66,
+    "rectY" : 100,
+    "rectWidth" : 18,
+    "rectHeight" : 23,
+    "rectRotate" : 180
+
+  },
+
+  "D" : {
+
+    "arcStart" : 0,
+    "arcEnd" : 360,
+
+    "circX" : 71,
+    "circY" : 100,
+    "circSize" : 19.5,
+
+    "rectX" : 79,
+    "rectY" : 80,
+    "rectWidth" : 6,
+    "rectHeight" : 95,
+    "rectRotate" : 180
 
   }
 
